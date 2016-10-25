f3d1a52cb25c9ecb7fd29c89f6c91852b55b0597
alejandro <realdeal@Vishals-MacBook-Pro.local>
2016-10-24 18:57:45 -0700

    added methods to controllers and did routes

--- project/app/controllers/allergies_controller.rb
+++ project/app/controllers/allergies_controller.rb
@@ -1,2 +1,10 @@
 class AllergiesController < ApplicationController
+
+  def create
+    @allergy = Allergy.create(content: params[:content], recipe_id: params[:recipe_id])
+  end
+
+  def destroy
+    @allergy = Allergy.find(:id).destroy
+  end
 end
--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def new
+    redirect_to "/users/new"
+  end
+
+  def create
+    @user = User.create(alias: params[:alias], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], phone: params[:phone], birthday: params[:birthday])
+    redirect_to "/recipes/home"
+  end
+
+  def destroy
+    @user = User.find(:id)
+    redirect_to "/recipes/home"
+  end
 end
